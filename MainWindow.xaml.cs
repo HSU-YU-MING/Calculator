6e6294a19d2b6567357b8c8059443be6f9593ef4
HSU-YU-MING <tmhaorm@gmail.com>
2023-04-17 14:16:40 +0800

    2023/04/17 上課
    1.0到9程式碼輸入完成
    2.修改完0到9程式碼

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,47 +27,71 @@ namespace Calculator
 
         private void btnOne_Click(object sender, RoutedEventArgs e)
         {
-
+            if (txtNumber.Text == "0")
+                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            txtNumber.Text += "1";
+            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnTwo_Click(object sender, RoutedEventArgs e)
         {
-
+            if (txtNumber.Text == "0")
+                txtNumber.Text = ""; 
+            txtNumber.Text += "2";
         }
 
         private void btnThree_Click(object sender, RoutedEventArgs e)
         {
-
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text += "3";
         }
 
         private void btnFour_Click(object sender, RoutedEventArgs e)
         {
-
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text += "4";
         }
 
         private void btnFive_Click(object sender, RoutedEventArgs e)
         {
-
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text += "5";
         }
 
         private void btnSix_Click(object sender, RoutedEventArgs e)
         {
-
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text += "6";
         }
 
         private void btnSeven_Click(object sender, RoutedEventArgs e)
         {
-
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text += "7";
         }
 
         private void btnEight_Click(object sender, RoutedEventArgs e)
         {
-
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text += "8";
         }
 
         private void btnNine_Click(object sender, RoutedEventArgs e)
         {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text += "9";
+        }
 
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "0";
         }
     }
 }
