15a3f4d3b406f5035aaa5ec5a2cea248db43224b
HSU-YU-MING <tmhaorm@gmail.com>
2023-04-17 13:56:19 +0800

    2023/04/17上課
    1.介面設計完成

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOne_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
